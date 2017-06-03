6ac173a2b7e0e589dfed76bf1197612cc7d17233
Alex Sikand <alex.sikand@viewglass.com>
2017-06-02 19:38:39 -0700

    Prettify code

--- js/firebaseLogoWrite.js
+++ js/firebaseLogoWrite.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-	
-	$( "#submitButton" ).click(function() {
+$(document).ready(function() {
+	$("#submitButton").click(function() {
 		var emailValue = $("#mail").val();
 		var domain = emailValue.substr(emailValue.indexOf("@") + 1);
 		var logoURL = "https://logo.clearbit.com/" + domain;
@@ -9,13 +8,9 @@ $(document).ready(function(){
 		console.log(logoURL);
 		console.log(firebase);
 		var configRef = firebase.database().ref('controllers/vgc123/config');
-		configRef.update({'/logoImage': logoURL});
+		configRef.update({
+			'/logoImage': logoURL
+		});
 		window.location.replace("./success/index.html")
-		
 	});
-	
-	
 });
-
-
-
