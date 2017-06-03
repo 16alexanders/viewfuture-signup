1fa7c3de9e72c244d251807bc9958748342ce24b
Alex Sikand <alex.sikand@viewglass.com>
2017-06-02 19:54:18 -0700

    Updates name AND logo URL in firebase, add delay before redirect

--- js/firebaseLogoWrite.js
+++ js/firebaseLogoWrite.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$("#submitButton").click(function() {
 		var emailValue = $("#mail").val();
+		var name = $("#name").val();
 		var domain = emailValue.substr(emailValue.indexOf("@") + 1);
 		var logoURL = "https://logo.clearbit.com/" + domain;
 		console.log(emailValue);
@@ -8,9 +9,8 @@ $(document).ready(function() {
 		console.log(logoURL);
 		console.log(firebase);
 		var configRef = firebase.database().ref('controllers/vgc123/config');
-		configRef.update({
-			'/logoImage': logoURL
-		});
-		window.location.replace("./success/index.html")
+		configRef.update( {'/logoImage': logoURL} );
+		configRef.update( {'/nameTitle': name} );
+		window.setTimeout('window.location.replace("./success/index.html")',1600);
 	});
 });
