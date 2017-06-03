8a18085ff768e89d131d57c550384a0f430aad08
Alex Sikand <alex.sikand@viewglass.com>
2017-06-02 17:03:44 -0700

    Fix firebase database write ability

--- js/firebaseLogoWrite.js
+++ js/firebaseLogoWrite.js
@@ -1,13 +1,17 @@
-$( "#submitButton" ).click(function() {
- 	var emailValue = $("#mail").val();
-	var domain = emailValue.substr(emailValue.indexOf("@") + 1);
-	console.log(domain);
-	var logoURL = "https://logo.clearbit.com/" + domain;
-	const functions = require('firebase-functions');
-	var admin = require("firebase-admin");
-	admin.initializeApp(functions.config().firebase);
-	var configRef = admin.database().ref('controllers/vgc123/config');
-	configRef.update( {"logoImage": logoURL} );
+$(document).ready(function(){
+
+	$( "#submitButton" ).click(function() {
+		alert("update logo run");
+		var emailValue = $("#mail").val();
+		var domain = emailValue.substr(emailValue.indexOf("@") + 1);
+		var logoURL = "https://logo.clearbit.com/" + domain;
+		console.log(emailValue);
+		console.log(domain);
+		console.log(logoURL);
+		console.log(firebase);
+		var configRef = firebase.database().ref('controllers/vgc123/config');
+		configRef.update({'/logoImage': logoURL});
+	});
 });
 
 
